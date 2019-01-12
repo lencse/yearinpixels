7cd35260765715ea9452213b81bec2da41b93b65
Levente Löki <leventeloki@gmail.com>
2019-01-12 12:57:41 +0100

    ?

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@
 if [ "$HEROKU" = "true" ]; then
     echo "Build on Heroku"
     yarn build
-    DATABASE_CONNECTION_URL=$DATABASE_URL unset DATABASE_URL db-migrate --config db-config.js --env prod up
+    export DATABASE_CONNECTION_URL=$DATABASE_URL
+    unset DATABASE_URL
+    db-migrate --config db-config.js --env prod up
 fi
