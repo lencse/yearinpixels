4d4a68ffab1eec5aa6f629b29474882e78e29e00
Levente Löki <leventeloki@gmail.com>
2019-01-12 13:13:30 +0100

    ???

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+if [ "$DEPLOY_STARTED" = "true" ]; then
+    exit 0
+fi
+
 if [ "$HEROKU" = "true" ]; then
+    export $DEPLOY_STARTED="0"
     echo "Build on Heroku"
     yarn build
     export DATABASE_CONNECTION_URL=$DATABASE_URL
