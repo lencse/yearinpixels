131149fe790dbecd5b7ba30423fc176af997b66e
Levente Löki <leventeloki@gmail.com>
2019-01-13 15:29:16 +0100

    Material UI

--- src/frontend/components/Application.tsx
+++ src/frontend/components/Application.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react'
+import Button from '@material-ui/core/Button'
 
 export default class Application extends React.Component<{}, {}> {
 
     public render(): React.ReactNode {
         return (
-            <div>qqq</div>
+            <div>
+                <Button variant='contained' color='primary'>
+                    Test
+                </Button>
+            </div>
         )
     }
 
