043c9c67e681f8231b0341567b267131fb8be433
Levente Löki <leventeloki@gmail.com>
2019-01-19 18:08:04 +0100

    Refactor

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-if [ "$DEPLOY_STARTED" = "true" ]; then
+if [ "$HEROKU" != "true" ] || [ "$DEPLOY_STARTED" = "true" ] then
     exit 0
 fi
 
-if [ "$HEROKU" = "true" ]; then
-    echo "Build on Heroku"
-    export DEPLOY_STARTED="true"
-    yarn build
-    export DATABASE_CONNECTION_URL=$DATABASE_URL
-    unset DATABASE_URL
-    yarn add --dev pg-native
-    db-migrate --config db-config.js --env prod up
-    yarn remove pg-native
-fi
+echo "Build on Heroku"
+export DEPLOY_STARTED="true"
+yarn build
+export DATABASE_CONNECTION_URL=$DATABASE_URL
+unset DATABASE_URL
+yarn add --dev pg-native
+db-migrate --config db-config.js --env prod up
