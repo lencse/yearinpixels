ff29ab58932923422225aa002af121654fa14a17
Levente Löki <leventeloki@gmail.com>
2019-01-20 01:39:13 +0100

    ?

--- .circleci/setup-sonarcloud.sh
+++ .circleci/setup-sonarcloud.sh
@@ -3,7 +3,7 @@
 SONAR_CLI_ZIP=sonar-scanner-cli-3.3.0.1492-linux.zip
 SONAR_CLI_DIR=sonar-scanner-3.3.0.1492-linux
 
-wget https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/$SONAR_CLI_ZIP
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli//$SONAR_CLI_ZIP
 unzip $SONAR_CLI_ZIP
 
 SONAR_CLI_BIN=`pwd`/$SONAR_CLI_DIR/bin
