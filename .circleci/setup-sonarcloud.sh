02a771bd94eaefe39dbaf2bdb19d942a81fc375c
Levente Löki <leventeloki@gmail.com>
2019-01-20 01:29:37 +0100

    Sonarcloud

---
+++ .circleci/setup-sonarcloud.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SONAR_CLI_ZIP=sonar-scanner-cli-3.3.0.1492-linux.zip
+SONAR_CLI_DIR=sonar-scanner-3.3.0.1492-linux
+
+wget https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/$SONAR_CLI_ZIP
+unzip $SONAR_CLI_ZIP
+
+SONAR_CLI_BIN=`pwd`/$SONAR_CLI_DIR/bin
+
+echo "export PATH=$SONAR_CLI_BIN:\$PATH" >> $BASH_ENV
