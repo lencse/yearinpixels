bdaca5c2302e0297de49293ea911b26b1187f6a3
Levente Löki <leventeloki@gmail.com>
2019-01-22 20:11:25 +0100

    Test mood change

---
+++ test/frontend/unit/action/changeMoodTest.ts
@@ -0,0 +1,11 @@
+import changeCurrentDate from '../../../../src/frontend/action/changeCurrentDate'
+import { initial } from '../../../../src/frontend/state/ApplicationState'
+import changeMood from '../../../../src/frontend/action/changeMood';
+
+describe('changeMood', () => {
+    it('State is changed', () => {
+        const init = initial(new Date('2019-01-01'))
+        const newState = changeMood(1).transform(init)
+        expect(newState.days.get(new Date(new Date('2019-01-01'))).mood).toEqual(1)
+    })
+})
