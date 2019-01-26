2b4aef1860a385c101dbd0970d2b69135eabbff4
Levente Löki <leventeloki@gmail.com>
2019-01-26 10:12:31 +0100

    .

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     testEnvironment: 'node',
     testMatch: [
         '**/test/**/*Test.ts?(x)',
-        // '**/test/**/*Integration*.ts?(x)'
+        '**/test/**/*Integration*.ts?(x)'
     ],
     coverageDirectory: 'logs/jest',
     collectCoverage: true,
